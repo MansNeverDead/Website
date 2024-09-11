ff8e4618f6959616e8697978b71348ecc7d8ee7a
MansNeverDead <nagadioyetti@gmail.com>
2024-09-11 19:51:03 -0300

    added console log

--- Code/script.js
+++ Code/script.js
@@ -5,4 +5,5 @@ function buttonPressed(button) {
     // Find the paragraph and change its content
     const messageParagraph = document.getElementById('message');
     messageParagraph.textContent = 'Button has been pressed, please wait!';
+    console.log("button has been pressed");
 }
