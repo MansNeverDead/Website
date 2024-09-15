997d3fd351d66f8f34a6fa24153c3ed655712c2d
MansNeverDead <nagadioyetti@gmail.com>
2024-09-15 13:51:16 -0300

    removed php

--- Code/script.js
+++
@@ -1,5 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-const page = params.get('page');
-if (page === 'blog') {
-    window.location.href = 'Different Pages/blogpostPage.html';
-}
