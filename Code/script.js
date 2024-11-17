48d1dca745c843369ce260d86eac2a3cb71284d3
MansNeverDead <nagadioyetti@gmail.com>
2024-11-17 17:08:48 -0300

    testr

--- Code/script.js
+++ Code/script.js
@@ -1,13 +1,13 @@
 const links = [
-    "https://docs.google.com/forms/d/e/1FAIpQLSfiu8_8rp5oTYAORJi5dvtHXQwf0seB_GygX166bqsdY8AQHA/viewform?usp=sf_link",
-    "https://docs.google.com/forms/d/e/1FAIpQLScwwTn-isnvCFVdGrZ-2JKbkOajo4Cxa-UekGI8dx9S_HurqQ/viewform?usp=sf_link",
-    "https://docs.google.com/forms/d/e/1FAIpQLScA8H1sWrOH5lQa8qX4vihHaCkYizixns6iBdiFRoeryULnUw/viewform?usp=sf_link",
-    "https://docs.google.com/forms/d/e/1FAIpQLSeNCvp0fY85jIRFrdE2u5ChH8XBTkv4pbZ1mXtOmLUILRm2uw/viewform?usp=sf_link",
-    "https://forms.gle/p1TPstVtguoyWH637"
+    "https://forms.gle/9NKLBJo9q23zoybg7",
+    "https://forms.gle/hXFCis1V5be98sie7",
+    "https://forms.gle/bvtNVxqa6i8W6uU2A",
+    "https://forms.gle/cXfjCNvrawhi1YYa6",
+    "https://forms.gle/dQzDJqRGMD6Yupxj6"
 ];
 
 function updateLink() {
-    const startDate = new Date("2024-01-01T00:00:00-03:00"); // French Guiana is UTC-3
+    const startDate = new Date("2024-11-17T00:00:00-03:00"); // French Guiana is UTC-3
 
     const currentDate = new Date(new Date().toLocaleString("en-US", { timeZone: "America/Cayenne" }));
 
