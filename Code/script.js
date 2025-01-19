e9dbce0b2e4dab16541e3ad0a8051ea6675ad93b
MansNeverDead <nagadioyetti@gmail.com>
2025-01-19 09:33:31 -0300

    changed forms

--- Code/script.js
+++ Code/script.js
@@ -1,14 +1,14 @@
 const links = [
-    "https://forms.gle/9NKLBJo9q23zoybg7",
-    "https://forms.gle/hXFCis1V5be98sie7",
-    "https://forms.gle/bvtNVxqa6i8W6uU2A",
-    "https://forms.gle/cXfjCNvrawhi1YYa6",
-    "https://forms.gle/dQzDJqRGMD6Yupxj6"
+    "https://forms.gle/diTojrp47iCh5XZ3A",
+    "https://forms.gle/a8VL5LZaoiT1wqEB9https://forms.gle/a8VL5LZaoiT1wqEB9",
+    "https://forms.gle/huVLiFGhpWzfw2NS7",
+    "https://docs.google.com/forms/d/e/1FAIpQLSfeghoRF36Ww-NOnBh4eThPTXR4N6of8ShR4AIrub4ll7B4dw/viewform?usp=dialog",
+    "https://forms.gle/xaCNX6CYQSc1dSBFA"
 ];
 
 function updateLink() {
     // Define start date for rotation
-    const startDate = new Date("2024-11-17T00:00:00-03:00"); // French Guiana is UTC-3
+    const startDate = new Date("2025-01-20T00:00:00-03:00");
 
     // Get the current date adjusted for French Guiana's timezone
     const currentDate = new Date(new Date().toLocaleString("en-US", { timeZone: "America/Cayenne" }));
@@ -17,10 +17,10 @@ function updateLink() {
     const daysSinceStart = Math.floor((currentDate - startDate) / (1000 * 60 * 60 * 24));
 
     // Select the link based on the calculated day
-    const link = links[daysSinceStart % links.length];
-    
-    // Return or log the link instead of updating a DOM element
-    console.log("Today's link lol:", link);
+    const targetURL = links[daysSinceStart % links.length];
+
+    // Redirect to the target URL
+    window.location.href = targetURL;
 }
 
 // Call the function
